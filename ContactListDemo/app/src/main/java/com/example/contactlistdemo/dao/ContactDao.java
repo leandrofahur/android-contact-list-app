3128f639d0ce79467a598b02017698b1e49bbd58
Leandro Miranda Fahur Machado <leandro@Leandros-MacBook-Pro-2.local>
2021-11-19 19:30:59 -0800

    update some crud operation

--- ContactListDemo/app/src/main/java/com/example/contactlistdemo/dao/ContactDao.java
+++ ContactListDemo/app/src/main/java/com/example/contactlistdemo/dao/ContactDao.java
@@ -1,4 +1,49 @@
 package com.example.contactlistdemo.dao;
 
+import static androidx.room.OnConflictStrategy.IGNORE;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.contactlistdemo.model.Contact;
+
+import java.util.List;
+
+@Dao
 public interface ContactDao {
+    /*
+     * A DAO is an extensible interface where we implement the CRUD. Keep the method names simple and explicit for a clean and intuitive interface!!!
+     */
+
+    // Insertion
+
+    @Insert(onConflict = IGNORE)
+    void insertContact(Contact contact);
+
+    @Insert(onConflict = IGNORE)
+    void insertAllContacts(List<Contact> contactList);
+
+    // Deletion
+
+    @Delete
+    void deleteContact(Contact contact);
+
+    // in the ui there will be a delete contact list btn
+    @Query("DELETE FROM contacts")
+    void deleteAllContacts();
+
+    // Selection:
+
+    // in the ui there will be a show all btn
+    @Query("SELECT * FROM contacts")
+    List<Contact> getAllContacts();
+
+    // in the ui there will be a search btn that will search by name
+    @Query("SELECT * FROM contacts WHERE first_name=:firstName")
+    List<Contact> getContactByName(String firstName);
+
+
 }
